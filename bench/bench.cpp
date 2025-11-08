0dffa5271436378c96ce7b41ec6b028889918660
Borislav Stanimirov <b@ibob.bg>
2025-11-08 18:05:55 +0200

    bench both copies of ducc

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -4,6 +4,8 @@
 #include <ducc0/infra/threading.h>
 #include <ducc0/fft/fft.h>
 
+#include <ducc-par/fft/fft.h>
+
 #include <iostream>
 #include <atomic>
 #include <random>
@@ -25,29 +27,47 @@ void warmup() {
     });
 }
 
-void orig(picobench::state& s) {
+template<typename T> void uc2c(
+    const ducc0::cfmav<std::complex<T>>& in,
+    const ducc0::vfmav<std::complex<T>>& out,
+    const std::vector<size_t>& axes
+) {
+    ducc0::c2c(in, out, axes, true, 1.f, NUM_THREADS);
+}
+
+template<typename T> void uc2c(
+    const ducc::par::cfmav<std::complex<T>>& in,
+    const ducc::par::vfmav<std::complex<T>>& out,
+    const std::vector<size_t>& axes
+) {
+    ducc::par::c2c(in, out, axes, true, 1.f, NUM_THREADS);
+}
+
+template <template <typename> class VFMAV>
+void bench(picobench::state& s) {
     auto& data = random_data_for_iterations[s.iterations()];
     std::vector<std::complex<float>> result(data.size());
 
     std::vector<size_t> axes = {0};
     std::vector<size_t> dims = {data.size()};
-    ducc0::vfmav<std::complex<float>> in(data.data(), dims);
-    ducc0::vfmav<std::complex<float>> out(result.data(), dims);
+    VFMAV<std::complex<float>> in(data.data(), dims);
+    VFMAV<std::complex<float>> out(result.data(), dims);
 
     {
         picobench::scope scope(s);
-        ducc0::c2c(in, out, axes, true, 1.f, NUM_THREADS);
+        uc2c(in, out, axes);
     }
 
     auto sum = std::accumulate(result.begin(), result.end(), std::complex<float>{0});
     s.set_result(std::bit_cast<uint32_t>(std::abs(sum)));
 }
 
-PICOBENCH(orig);
+PICOBENCH(bench<ducc0::vfmav>).label("original");
+PICOBENCH(bench<ducc::par::vfmav>).label("par");
 
 int main(int argc, char* argv[]) {
     picobench::runner r;
-    r.set_default_state_iterations({1000, 10'000});
+    r.set_default_state_iterations({10'000, 50'000});
     r.set_compare_results_across_samples(true);
     r.set_compare_results_across_benchmarks(true);
     r.parse_cmd_line(argc, argv);
